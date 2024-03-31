6bec5c71b4e6b5d286977b78b6fb050f5a5c7c7e
Reza Abedi <reza.abd95@yahoo.com>
2024-03-31 03:42:14 +0330

    feat(Products Page) : added data's products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -4,7 +4,20 @@ import { useProducts } from "../context/ProductContext";
 function ProductsPage() {
   const products = useProducts();
   console.log(products);
-  return <div>ProductsPage</div>;
+  return (
+    <div>
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <p>{product.title}</p>
+            <div>{product.price}$</div>
+            <br />
+          </div>
+        ))}
+      </div>
+      <div>sidebar</div>
+    </div>
+  );
 }
 
 export default ProductsPage;
