aa81e1c4966e3d5af0c0f7a5af0682643584a71b
Reza Abedi <reza.abd95@yahoo.com>
2024-04-09 04:11:15 +0330

    feat(Products Page) : added searchbox and categories without logic

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,66 @@
-import React from "react";
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+
+import React, { useEffect, useState } from "react";
 import { useProducts } from "../context/ProductContext";
 import Card from "../components/Card";
 import LoaderSpinner from "../components/LoaderSpinner";
 
 function ProductsPage() {
   const products = useProducts();
-  console.log(products);
+
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+
+  const handleSearch = () => {
+    console.log("search");
+  };
+
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = e.target.innerText.toLowerCase();
+    if (tagName !== "LI") return;
+    console.log(category, tagName);
+  };
+
   return (
-    <div className="flex justify-between">
-      <div className="w-full flex  flex-wrap justify-between">
-        {!products.length && <LoaderSpinner />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={handleSearch}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className="flex justify-between">
+        <div className="w-full flex  flex-wrap justify-between">
+          {!displayed.length && <LoaderSpinner />}
+          {displayed.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>
+          <div>
+            <FaListUl /> <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler} className="[&>li]:cursor-pointer">
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
