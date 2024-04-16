92d7b46f40fd32e8e89c188a15a5599057c0d06e
Reza Abedi <reza.abd95@yahoo.com>
2024-04-16 04:00:01 +0330

    feat(sidebar):added styles

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -11,11 +11,14 @@ function SideBar({ setQuery }) {
     if (tagName !== "LI") return;
   };
   return (
-    <div>
-      <div>
-        <FaListUl /> <p>Categories</p>
+    <div className="w-64 h-fit text-lg ml-7 m-3 p-5 border-2 border-dashed border-[#e2e2e2] rounded-2xl">
+      <div className="flex items-center mb-2 text-red-500 font-medium">
+        <FaListUl /> <p className="ml-2">Categories</p>
       </div>
-      <ul onClick={categoryHandler} className="[&>li]:cursor-pointer">
+      <ul
+        onClick={categoryHandler}
+        className="[&>li]:cursor-pointer [&>li]:p-1 [&>li]:list-none [&>li:hover]:text-red-500 "
+      >
         <li>All</li>
         <li>Electronics</li>
         <li>Jewelery</li>
