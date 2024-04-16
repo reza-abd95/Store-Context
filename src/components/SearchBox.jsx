087790dae9253542f8ef87095bb2616734a430d2
Reza Abedi <reza.abd95@yahoo.com>
2024-04-16 03:39:46 +0330

    feat(searchbox): added styles

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -8,14 +8,18 @@ function SearchBox({ search, setSearch, setQuery }) {
     setSearch("");
   };
   return (
-    <div>
+    <div className="mx-2 mb-12">
       <input
         type="text"
         placeholder="search"
         value={search}
         onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        className="mr-2 border-dashed border-2 border-red-500 p-2 w-64 text-base text-red-500 rounded-lg focus:outline-none"
       />
-      <button onClick={handleSearch}>
+      <button
+        onClick={handleSearch}
+        className="bg-red-500 text-white border-none text-base p-2 rounded-lg cursor-pointer"
+      >
         <ImSearch />
       </button>
     </div>
